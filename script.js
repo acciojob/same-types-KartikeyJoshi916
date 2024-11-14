c89900548ca4c66204014f9c70361ffec6f9e4e0
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-11-14 18:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
-	if (isNaN(value1) == NaN && isNaN(value2) == NaN){return NaN}
+	if (isNaN(value1) == NaN && isNaN(value2) == NaN){return true
+													 }
 	return typeof value1==typeof value2;
 }
 
